b30648966599f7266ebe336b34591680fcf52f52
cledant <cledant@student.42.fr>
2017-01-20 19:17:59 +0100

    WIP show_alloc_mem

--- includes/show_alloc_mem.h
+++ includes/show_alloc_mem.h
@@ -6,7 +6,7 @@
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/20 17:02:18 by cledant           #+#    #+#             */
-/*   Updated: 2017/01/20 17:03:57 by cledant          ###   ########.fr       */
+/*   Updated: 2017/01/20 18:58:38 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "malloc_struct.h"
 
-void	show_alloc_mem();
+void	show_alloc_mem(void);
+void	sam_print_base(size_t num, char *base);
 
 #endif
--- srcs/sam_print_base.c
+++ srcs/sam_print_base.c
@@ -1,18 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   show_alloc_mem.c                                   :+:      :+:    :+:   */
+/*   sam_print_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cledant <cledant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/20 17:09:29 by cledant           #+#    #+#             */
-/*   Updated: 2017/01/20 19:00:03 by cledant          ###   ########.fr       */
+/*   Created: 2017/01/20 18:44:40 by cledant           #+#    #+#             */
+/*   Updated: 2017/01/20 19:10:17 by cledant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-void	show_alloc_mem(void)
+void	sam_print_base(size_t num, char *base)
 {
-	return ;
+	char	display[256];
+	size_t	size_base;
+	size_t	i;
+
+	if (num == 0)
+		return (ft_putchar('0'));
+	if ((size_base = ft_strlen(base)) == 1)
+		return ;
+	ft_bzero(display, 256);
+	i = 0;
+	while (num != 0)
+	{
+		display[i] = base[num % size_base];
+		num /= size_base;
+		i++;
+	}
+	while (i != 0)
+	{
+		ft_putchar(display[i - 1]);
+		i--;
+	}
 }
